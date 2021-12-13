62d15df2720735d0152ce31f4f31eb06f5726750
DanieleFic <reb_ficus22@hotmail.it>
2021-12-13 15:05:53 +0100

    fatto esercizio dadi

--- js-dadi/js/myscript.js
+++ js-dadi/js/myscript.js
@@ -1,1 +1,12 @@
-const mail = prompt (" Inserici Email "); //Inserisco il prompt in cui l'utente inserisce l'email
+
+let dadoUtente = (Math.floor (Math.random()*6+1));
+let  dadoAI =  (Math.floor (Math.random()*6+1));
+
+console.log(dadoUtente)
+console.log(dadoAI)
+
+if(dadoUtente > dadoAI){
+    document.getElementById("risultato").innerHTML = "Hai vinto"
+}else{
+    document.getElementById("risultato").innerHTML = "AI ha vinto"
+}
