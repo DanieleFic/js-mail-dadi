caddec418edb8e2d158460240de212f5d1c708e2
DanieleFic <reb_ficus22@hotmail.it>
2021-12-13 15:41:58 +0100

    fatto esercizio dadi

--- js-dadi/js/myscript.js
+++ js-dadi/js/myscript.js
@@ -1,16 +1,17 @@
 
-let dadoUtente = (Math.floor (Math.random()*6+1));
-let  dadoAI =  (Math.floor (Math.random()*6+1));
+let dadoUtente = (Math.floor (Math.random()*6+1)); //facciamo la variabile del lancio del dado da 1 a 6 
+let  dadoAI =  (Math.floor (Math.random()*6+1));   //facciamo la variabile del lancio del dado da 1 a 6 
 
-console.log(dadoUtente)
+console.log(dadoUtente) //stampiamo sulla console log il risultato del lancio dei dadi dell utente e dell AI
 console.log(dadoAI)
 
-if(dadoUtente > dadoAI){
-    document.getElementById("risultato").innerHTML = "Hai vinto"
+
+if(dadoUtente > dadoAI){    //specificiamo che se  il risultato del lancio del dado dell utente è maggiore del numero del dado dell AI vince l'utente 
+    document.getElementById("risultato").innerHTML = "Utente rolla"  +" ,"+ dadoUtente +" "+  "AI rolla"  +" "+ dadoAI  +" ,"+ "Utente ha vinto"
 }
-if(dadoUtente == dadoAI){
-    document.getElementById("risultato").innerHTML = "Pari, ritentate"
+else if(dadoUtente == dadoAI){ //se il risultato del lancio del dado è pari nessuno vince 
+    document.getElementById("risultato").innerHTML = "Utente rolla"  +" ,"+ dadoUtente +" "+  "AI rolla"  +" "+ dadoAI  +" ,"+ "Pari, ritenta"
 }
-else{
-    document.getElementById("risultato").innerHTML = "AI ha vinto"
+else{ //se il risultato dei dadi è a favore dell AI vince lei
+    document.getElementById("risultato").innerHTML = "Utente rolla"  +" ,"+ dadoUtente +" "+ "AI rolla"  +" "+ dadoAI  +" ,"+ "AI ha vinto"
 }
